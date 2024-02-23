f5aab829ee8c78c99aee9869bf6ab270bfa38c2f
satos <satos@wylog.com>
2024-02-24 07:45:06 +0800

    Added pages

---
+++ frontend/src/views/HomePage.jsx
@@ -0,0 +1,11 @@
+function HomePage() {
+    return (
+        <div className="App">
+            <h1>Hello World</h1>
+            <a href="login">Login</a>
+        </div>
+    )
+
+}
+
+export default HomePage;
---
+++ frontend/src/views/LoginPage.jsx
@@ -0,0 +1,11 @@
+function LoginPage() {
+    return (
+        <div className="App">
+            <h1>Login Page</h1>
+            <a href="/">Home page</a>
+        </div>
+    )
+
+}
+
+export default LoginPage;
